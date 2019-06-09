d068cc4e8eeb342accbd308c6265f877d890d1cb
kya9kyo2280 <kya9kyo2280@gmail.com>
2019-06-09 18:32:56 +0900

    add7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,8 +20,20 @@ class BlogsController < ApplicationController
   	redirect_to blogs_path
   end
 
-def edit
+  def edit
     @blog = Blog.find(params[:id])
+  end
+
+  def destroy
+      blog = Blog.find(params[:id])
+      blog. destroy
+      redirect_to blogs_path
+  end
+
+  def update
+      blog = Blog.find(params[:id])
+      blog.update(blog_params)
+      redirect_to blog_path(blog)
   end
 
   private
@@ -32,9 +44,5 @@ def edit
 
   
 
-  def update
-  	  blog = Blog.find(params[:id])
-  	  blog.update(blog_params)
-  	  redirect_to blog_path(blog)
-  end
+  
 end
